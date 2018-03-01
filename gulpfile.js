83ed1488f437b7116305d415575c131ce7458e5d
jack <1395093509@qq.com>
2018-03-01 11:12:20 +0800

    更新

--- gulpfile.js
+++ gulpfile.js
@@ -40,6 +40,18 @@ var qnOptions = {
   origin: qnConfig.qnDomainUIS,
 }
 
+var AUTOPREFIXER_BROWSERS = [
+  'ie >= 9',
+  'ie_mob >= 10',
+  'ff >= 30',
+  'chrome >= 34',
+  'safari >= 7',
+  'opera >= 23',
+  'ios >= 7',
+  'android >= 2.3',
+  'bb >= 10',
+];
+
 // --path=xxx
 // copy src/xxx/json  -> dist/xxx
 // md5 src/xxx/**/* (json) -> dist/xxx
@@ -60,6 +72,23 @@ var paths = {
 // del
 gulp.task('clean', function(cb) {
   return del('dist');
+});
+
+gulp.task('test', function(cb) {
+  var css = '.ztn-show-tip-box{height: 100%;overflow: hidden;}.ztn-show-tip-box img{width:100%;height:100%;}.ztn-tip-box{height:100%;position:relative;}.ztn-show-tip-box img{height:100%;}.ztn-tip-box .cancel{position: absolute;right: 0;top: 5%;width: 12%;height: 7%;}.ztn-tip-content{position:absolute;top: 57.5%;left: 11%;width: 79%;height: 20%;}.ztn-order-mask{z-index:2000;content: '';position: absolute;bottom: -25px;left: 0;width: 100%;height: 20px;box-shadow: 0px -15px 15px 5px #fff;}.ztn-lottery-scroll{height: 100%;width: 100%;overflow-y: scroll;padding-bottom: 21px;}.ztn-tip-content .ztn-input-tel{border: none;width: 100%;text-align: center;height: 30%;border-radius: 5px;margin-bottom: 2%;}.ztn-get-codebox{margin-bottom: 2%;overflow: hidden;height: 28%;width: 100%;}.ztn-tip-content .ztn-input-code{border: none;width: 65%;text-align: center;height: 100%;border-radius: 5px;margin-right: 5%;float: left;}.ztn-get-code{float: left;width: 30%;height: 100%;background-color: #fee53c;border-radius: 6px;text-align: center;font-size: 12px;display: flex;flex-direction: column;justify-content: center;}.ztn-receive-btn{width: 100%;height: 30%;text-align: center;display: flex;flex-direction: column;justify-content: center;background-color: #fee53c;border-radius: 5px;color: #ed0000;font-weight: bold;font-size: 16px;}.ztn-coupon-desc{margin-bottom: 5%;height: 50%;display: flex;justify-content: center;color: #6d1398;}.ztn-coupon-desc .ztn-coupon-price{float: left;font-size: 77px;font-weight: 500;}.ztn-coupon-desc .ztn-coupon-box{float: left;display: flex;flex-direction: column-reverse;}.ztn-coupon-box .ztn-coupon-text{    font-size: 16px;font-weight: bold;margin-bottom: -6px;}.ztn-coupon-box .ztn-coupon-condition{font-size: 15px;}.ztn-go-use,.ztn-add-package{width: 60%;height: 20%;margin-left: 20%;background-color: #fff;border-radius: 1000px;text-align: center;color: #6d1398;font-weight: bold;font-size: 15px;display: flex;justify-content: center;flex-direction: column;margin-bottom: 3%;}.ztn-tip-content .ztn-none-order{width: 100%;height: auto;margin-top: 5%;}.ztn-get-codebox .disabled{background-color:#d2d2d2;}.ztn-tip-sure-box{position:relative;display:none;}.ztn-tip-sure-box .ztn-sure-btn{position: absolute;left: 28%;bottom: 33%;height: 5%;width: 44%;}.ztn-lottery-order{overflow:hidden;margin-bottom: 1%;}.ztn-lottery-order .ztn-order-tit{float:left;font-weight: 600;}.ztn-lottery-order .ztn-order-creatat{float:right;margin-right: 4%;}.ztn-mask{display:none;position:fixed;left:0;top:0;height:100%;width:100%;z-index:1001;}';
+  var cleaner = postcss([
+    autoprefixer({
+      add: false,
+      browsers: AUTOPREFIXER_BROWSERS,
+    }),
+  ]);
+  var prefixer = postcss([ autoprefixer ]);
+
+  cleaner.process(css).then(function (cleaned) {
+      return prefixer.process(cleaned.css);
+  }).then(function (result) {
+      console.log(result.css);
+  });
 });
 
 // copy
@@ -85,7 +114,7 @@ gulp.task('md5', ['copy'], function(){
   // console.log(content.toString());
 });
 
-gulp.task('md5json', function(){
+gulp.task('md5json', ['copy'], function(){
   pump([
     gulp.src(paths.quoteSrc),
     $.replace(/.\/img\//g, qnOptions.origin + 'zt/' + dir + '/img/'),
